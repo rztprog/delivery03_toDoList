ec2a72024079a0c101f9b9bb07db015470989ec0
rztprog <rztprog@outlook.fr>
2019-04-05 17:03:28 +0200

    adding some test js

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,20 @@
 'use strict';
 
+// Target the UL with his class
+let ul = document.body.getElementsByClassName("ul")[0];
 
+// Function for create a new li
+let newLi = document.createElement("li"); 
+
+// Target the input with his class
+let input = document.querySelector("input");
+
+// Add the newLi with the text "Cheese"
+newLi.innerHTML = "Cheese";
+
+function add(){
+	ul.appendChild(newLi);
+}
+
+
+	
