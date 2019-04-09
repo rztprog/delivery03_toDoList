658fa3faada760b65d5966faa6e12a596c11a124
rztprog <rztprog@outlook.fr>
2019-04-09 11:08:34 +0200

    clean file (separate function) & add redfield to empty field & same li & add line-through

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,7 @@
 'use strict';
 
 ////////// VARIABLES //////////
+///////////////////////////////
 
 // Target the UL
 let ul = document.getElementsByClassName("ul")[0];
@@ -17,34 +18,48 @@ dark.onclick = darkmode;
 
 
 ////////// FUNCTIONS //////////
+///////////////////////////////
+function upper(str){
+	str = str.split("");
+	return str[0] = str[0].toUpperCase() + str.splice(1).join("");
+};
+
+function redField(){
+	datastyle.backgroundColor = "rgb(255, 55, 55)";
+	//li.style.backgroundColor = "rgb(255, 55, 55)"; 
+	return setTimeout( () => datastyle.backgroundColor = (i==0) ? "rgba(170, 170, 170, 0.9)" : "rgba(230, 230, 230, 0.9)", 500);
+};
+
 function add(){
 
 	let newLi = document.createElement("li"); 
 	let lis = document.getElementsByTagName("li");
 	let data = document.body.getElementsByClassName("data")[0].value;
 	if (data == "" || data == undefined){
-		return alert("DONT PUSH this button when he's empty, everything could EXPLODE !");
+		return redField();
 	}
 	
-	function upper(str){
-		let newStr = str.split("");
-		newStr[0] = newStr[0].toUpperCase();
-		return newStr.join("");
-	}
-
 	data = upper(data);
 	newLi.textContent = data;
 	
 	for(let li of lis){ 
-		if( newLi.textContent == li.innerHTML ){
-			datastyle.backgroundColor = "rgb(255, 55, 55)";
-			//li.style.backgroundColor = "rgb(255, 55, 55)"; 
-			return setTimeout( () => datastyle.backgroundColor = "rgba(170, 170, 170, 0.9)", 500);
+		if( newLi.textContent == li.textContent){
+			return redField();
 		}
+	}
+
+	newLi.onclick = function(){
+		newLi.style.textDecoration = "line-through";
+		// newLi.style.color = "rgba(0, 0, 0, 0.6)";
 	}
 
 	ul.appendChild(newLi);
 };
+
+// function fadein(){
+// 	newLi.style.textDecoration = "line-through";
+// 	(i == 1) ? newLi.style.color = "rgba(0, 0 , 0, 0.1)" : newLi.style.color = "rgba(255, 255, 255, 1)";	
+// }
 
 function darkmode(){
 	if(i == 1){ 	// DARK
@@ -69,7 +84,8 @@ function darkmode(){
 };
 
 
-//////// EVENT ////////
+///////////// EVENT ////////////
+///////////////////////////////
 // dataKey.onkeypress = function(event) {
 // 	if(event.keyCode == 13){
 // 		event.preventDefault();
